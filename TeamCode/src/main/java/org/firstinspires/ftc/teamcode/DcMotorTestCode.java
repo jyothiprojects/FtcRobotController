ee05f2ab4b1761f577f6ce11d5df84b2eac72039
nexfind <nexfind@gmail.com>
2025-11-22 14:47:56 -0600

    DcMotor test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DcMotorTestCode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class DcMotorTestCode extends OpMode {
+
+    public DcMotor motor;
+    double speed = 0.5;
+
+    @Override
+    public void init (){
+        motor = hardwareMap.get(DcMotor.class, "single_motor");
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(speed);
+    }
+}
